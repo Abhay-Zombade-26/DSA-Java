3d6360ecb561b8b6d8457f964aba983dc2dcf41d
Abhay9326397439 <abhayzombade26@gmail.com>
2026-01-10 13:55:22 +0530

    advance pattern2

---
+++ pattern_logic/demo8.java
@@ -0,0 +1,16 @@
+package pattern_logic;
+
+public class demo8 {
+    public static void main(String[] args) {
+        int row=5;
+        for(int i=1;i<=row;i++){
+           for(int j=1;j<=row-i;j++){
+            System.out.print(" ");
+           }
+           for(int j=1;j<=row;j++){
+            System.out.print("*");
+           }
+           System.out.println();
+        }
+    }
+}
