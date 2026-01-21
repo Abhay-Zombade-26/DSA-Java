8a7e66622e484f984bd71fee1ccd71ca37b40781
Abhay9326397439 <abhayzombade26@gmail.com>
2026-01-21 23:05:35 +0530

    Sorting an array of even and odd wise

--- Arrays/demo16.java
+++ Arrays/demo16.java
@@ -40,3 +40,42 @@ class demo16{
         show(arr);
     }
 }
+
+//Sorting an array of even and odd numbers.
+
+// import java.util.*;
+
+// class demo16{ 
+//     public static void swap(int arr[],int i,int j){
+//         int temp=arr[i];
+//         arr[i]=arr[j];
+//         arr[j]=temp;
+//     }
+//     public static void display(int arr[]){
+//         int j=arr.length-1;
+//         int i=0;
+//         while(i<j){
+//             if(arr[i]%2!=0 && arr[j]%2==0){
+//                 swap(arr,i,j);
+//                 i++;
+//                 j--;
+//                }
+//                if(arr[i]%2==0){
+//                    i++;
+//                }else if(arr[j]%2!=0){
+//                    j--;
+//                }
+//             }
+//         }
+//     public static void main(String[] args) {
+//         int arr[]={10,26,15,21,30,45};
+//         System.out.println("O.G. array");
+//         System.out.println(Arrays.toString(arr));
+//         display(arr);
+//         System.out.println("new array");
+//         System.out.println(Arrays.toString(arr));
+//     }
+// }
+
+
+
