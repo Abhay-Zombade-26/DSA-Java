b97f1f5a719fd922b3d61d80682354183c6c097a
Abhay9326397439 <abhayzombade26@gmail.com>
2026-01-22 22:29:40 +0530

    without using reverse function

--- Arrays/demo17.java
+++ Arrays/demo17.java
@@ -4,6 +4,58 @@ package Arrays;
 
 import java.util.*;
 
+// public class demo17 {
+
+//     public static void display(int arr[]){
+//         for(int i=0;i<arr.length;i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//         System.out.println();
+//     }
+
+//     public static void reverse(int arr[]){
+//          int j=arr.length-1;
+//          int i=0;
+//          while(j>i){
+//             int temp=arr[i];
+//             arr[i]=arr[j];
+//             arr[j]=temp;
+//             i++;
+//             j--;
+//          }
+//     }
+
+//     public static int[] square(int arr[]){
+//        int i=0;
+//        int j=arr.length-1;
+//        int narr[]=new int[arr.length];
+//        int k=0;
+//        while(j>=i){
+//         if(Math.abs(arr[j])<Math.abs(arr[i])){
+//            narr[k++]=arr[i]*arr[i];
+//            i++;
+//         }else{
+//             narr[k++]=arr[j]*arr[j];
+//             j--;
+//         }
+//        }
+//        return narr;
+//     }
+//     public static void main(String[] args) {
+//         int arr[]={-10,-5,-2,2,4,5};
+//         System.out.println("O.G. array");
+//         display(arr);
+//         System.out.println("new arr");
+//         int brr[]=square(arr);
+//         reverse(brr);
+//         System.out.println(Arrays.toString(brr));
+//     }
+// }
+
+
+//without using reverse function.
+
+
 public class demo17 {
 
     public static void display(int arr[]){
@@ -13,41 +65,29 @@ public class demo17 {
         System.out.println();
     }
 
-    public static void reverse(int arr[]){
-         int j=arr.length-1;
-         int i=0;
-         while(j>i){
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-            i++;
-            j--;
-         }
-    }
 
     public static int[] square(int arr[]){
        int i=0;
        int j=arr.length-1;
        int narr[]=new int[arr.length];
-       int k=0;
+       int k=j;
        while(j>=i){
         if(Math.abs(arr[j])<Math.abs(arr[i])){
-           narr[k++]=arr[i]*arr[i];
+           narr[k--]=arr[i]*arr[i];
            i++;
         }else{
-            narr[k++]=arr[j]*arr[j];
+            narr[k--]=arr[j]*arr[j];
             j--;
         }
        }
        return narr;
     }
     public static void main(String[] args) {
-        int arr[]={-10,-5,-2,2,4,5};
+        int arr[]={-10,-5,-2,3,4,7};
         System.out.println("O.G. array");
         display(arr);
         System.out.println("new arr");
         int brr[]=square(arr);
-        reverse(brr);
         System.out.println(Arrays.toString(brr));
     }
 }
