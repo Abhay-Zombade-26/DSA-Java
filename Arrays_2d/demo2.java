523ece90c1f53c65f442fa1f824583d0b2790cb4
Abhay9326397439 <abhayzombade26@gmail.com>
2026-02-01 12:21:42 +0530

    Optimized transpose matrix solution

--- Arrays_2d/demo2.java
+++ Arrays_2d/demo2.java
@@ -1,10 +1,12 @@
-//Accept the r*c matrix and print that matrix transpose.
+//Accept the r*c square matrix and print that matrix transpose without using extra space.
+//Time complexity: O(n²)  space complexity:O(1)
 
 package Arrays_2d;
 
 import java.util.*;
 
-public class demo2 {
+public class demo2{
+
     public static void display(int arr[][]){
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr[i].length;j++){
@@ -13,32 +15,78 @@ public class demo2 {
             System.out.println();
         }
     }
-    public static void transpose(int arr1[][],int r1,int c1){
-        int narr[][]=new int[c1][r1];
-          for(int i=0;i<c1;i++){
-            for(int j=0;j<r1;j++){
-                narr[i][j]=arr1[j][i];
+
+    public static void swapArray(int arr[][],int r1,int c1){
+          for(int i=0;i<r1;i++){
+            for(int j=i+1;j<c1;j++){            
+                //without temp                                      // with temp variable         
+               arr[i][j]=arr[i][j]+arr[j][i];                      // int temp=arr[i][j];
+               arr[j][i]=arr[i][j]-arr[j][i];                     // arr[i][j]=arr[j][i];
+               arr[i][j]=arr[i][j]-arr[j][i];                    // arr[j][i]=temp;
             }
         }
-        display(narr);
+        display(arr);
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter value for r1 and c1:");
+        System.out.println("Enter value for r1 and c1");
         int r1=sc.nextInt();
         int c1=sc.nextInt();
-        int arr1[][]=new int[r1][c1];
-       
-        System.out.println("enter array1 i/p:");
-        for(int i=0;i<arr1.length;i++){
-            for(int j=0;j<arr1[i].length;j++){
-                arr1[i][j]=sc.nextInt();
-             }
+        int arr[][]=new int[r1][c1];
+        System.out.println("enter the 2d array element");
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                arr[i][j]=sc.nextInt();
+            }
         }
-        
         System.out.println("O.G 2d array");
-        display(arr1);
+        display(arr);
         System.out.println("Transpose matrix");
-        transpose(arr1, r1, c1);
+        swapArray(arr, r1, c1);
     }
 }
+
+
+
+//Accept the r*c square matrix and print that matrix transpose using extra space.
+
+// import java.util.*;
+
+// public class demo2 {
+//     public static void display(int arr[][]){
+//         for(int i=0;i<arr.length;i++){
+//             for(int j=0;j<arr[i].length;j++){
+//                 System.out.print(arr[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+//     public static void transpose(int arr1[][],int r1,int c1){
+//         int narr[][]=new int[c1][r1];
+//           for(int i=0;i<c1;i++){
+//             for(int j=0;j<r1;j++){
+//                 narr[i][j]=arr1[j][i];
+//             }
+//         }
+//         display(narr);
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter value for r1 and c1:");
+//         int r1=sc.nextInt();
+//         int c1=sc.nextInt();
+//         int arr1[][]=new int[r1][c1];
+       
+//         System.out.println("enter array1 i/p:");
+//         for(int i=0;i<arr1.length;i++){
+//             for(int j=0;j<arr1[i].length;j++){
+//                 arr1[i][j]=sc.nextInt();
+//              }
+//         }
+        
+//         System.out.println("O.G 2d array");
+//         display(arr1);
+//         System.out.println("Transpose matrix");
+//         transpose(arr1, r1, c1);
+//     }
+// }
