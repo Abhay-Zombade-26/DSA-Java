91dcbf631fcc08d8584e0a911234cbb67329736c
Abhay9326397439 <abhayzombade26@gmail.com>
2026-02-18 23:38:41 +0530

    learned asymptotic notations and tried some time_complexity ex.,

--- Time_Space_complexity/demo1.java
+++ Time_Space_complexity/demo1.java
@@ -57,17 +57,80 @@ import java.util.*;
 
 //this code is the optimized solution ans for this question
 //Time =O(1)
-public class demo1 {
-    public static void display(int n){
-        int ans=0;
-        ans=n*(n+1)/2;
-      System.out.println("First n natural sum is:"+ans);
-    }
+// public class demo1 {
+//     public static void display(int n){
+//         int ans=0;
+//         ans=n*(n+1)/2;
+//       System.out.println("First n natural sum is:"+ans);
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter the n Number: ");
+//         int n=sc.nextInt();
+//         display(n);
+//     }
+// }
+
+//--------------------------------------------------------- Day 2 -------------------------------------------------------------------------------
+/* 
+Asymptotic Notation (define)
+Definition:
+Asymptotic notation is used to describe the performance (time or space) of an algorithm when the input size becomes very large (n → infinity).
+It shows how fast or slow an algorithm grows, not exact time.
+Types:-
+Big-O (O): Shows worst case time complexity of algorithm.
+Omega (Ω): Shows best case time complexity of algorithm.
+Theta (Θ): Shows average case time complexity of algorithm.
+
+| Notation | Case         | Meaning      |
+| -------- | ------------ | ------------ |
+| O(n)     | Worst case   | Maximum time |
+| Ω(n)     | Best case    | Minimum time |
+| Θ(n)     | Average case | Normal time  |
+
+*/
+
+//ex., Time complexity=O(n+m)
+
+// public class demo1{
+//     public static void main(String[] args) {
+//         int arr[]={1,2,3,4,5};
+//         int arr2[]={10,20,30,40,50};
+//         int ans=0;   //ignore this type of all instructions for calculating the time complexity
+//         for(int i=0;i<arr.length;i++){
+//             System.out.println(arr[i]+" ");  //this for loop block will execute till n time 
+//         }
+//         for(int j=0;j<arr2.length;j++){
+//             System.out.println(arr2[j]+" "); //this for loop block will execute till m time 
+//         }
+//     }                                        //final result is:- O(n+m)
+// }    
+
+//ex., Time complexity=O(n^2)
+
+// public class demo1{
+//     public static void main(String[] args) {
+//         int n=5;
+//         for(int i=1;i<=n;i++){      //this loop will run n times
+//             for(int j=1;j<=n;j++){   //this loop will run also n times 
+//                 System.out.print(i*j+" ");  //total time will require for result O(n*n)
+//            } 
+//            System.out.println();
+//         }
+//     }                                 
+// }    
+
+//ex., for Time complexity O(n*√n)
+public class demo1{
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the n Number: ");
-        int n=sc.nextInt();
-        display(n);
-    }
-}
+        int n=4;
+        for(int i=1;i<=n;i++){      //this loop will run n times
+            for(int j=1;j<=Math.sqrt(i);j++){   //this loop will run also root of n times 
+                System.out.print(i+" "+j+" ");  //total time will require for result O(n*√n)
+           } 
+           System.out.println();
+        }
+    }                                 
+}  
+
 
