e83f4100a2444af6bc9da7dbbf1f6de9716172d4
Abhay9326397439 <abhayzombade26@gmail.com>
2026-02-19 22:17:45 +0530

    sort ArrayList without using etc ArrayList

---
+++ ArrayList/demo7.java
@@ -0,0 +1,26 @@
+//sort the ArrayList.
+package ArrayList;
+
+import java.util.*;
+public class demo7 {
+    public static void sortList(ArrayList<Integer> l1) {
+        for(int i =0;i<l1.size()-1;i++) {
+            for(int j=0;j<l1.size()-1-i;j++) {
+                if(l1.get(j)>l1.get(j + 1)) {
+                    int temp = l1.get(j);
+                    l1.set(j, l1.get(j+1));
+                    l1.set(j+1,temp);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> l1 = new ArrayList<>();
+        l1.add(40);
+        l1.add(10);
+        l1.add(30);
+        l1.add(20);
+        sortList(l1);   
+        System.out.println("Sorted list: " + l1);
+    }
+}
