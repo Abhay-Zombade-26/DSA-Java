5737285e1cecd636d1391e8b8fb03e02ad976466
Abhay9326397439 <abhayzombade26@gmail.com>
2026-03-12 23:53:59 +0530

    Count Digits of a Number using recursion

---
+++ Recursion/demo5.java
@@ -0,0 +1,19 @@
+//Count Digits of a Number
+
+package Recursion;
+
+import java.util.*;
+
+public class demo5 {
+    public static int count(int n){
+        if(n == 0)
+            return 0;
+        return 1+count(n/10);
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Digits = " + count(n));
+    }
+}
