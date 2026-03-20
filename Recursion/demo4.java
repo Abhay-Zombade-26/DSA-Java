43ab30e42b6a954d4810ff7f4b6553ed7a63c0e9
Abhay9326397439 <abhayzombade26@gmail.com>
2026-03-20 23:37:07 +0530

    power of no. with optimized solution

--- Recursion/demo4.java
+++ Recursion/demo4.java
@@ -2,14 +2,46 @@
 
 package Recursion;
 
+// import java.util.*;
+
+// public class demo4{
+
+//     public static int display(int x, int n){
+//         if(n==0)
+//             return 1;
+//         return x*display(x, n-1);
+//     }
+
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter base: ");
+//         int x = sc.nextInt();
+//         System.out.print("Enter power: ");
+//         int n = sc.nextInt();
+//         System.out.println("Result = "+display(x,n));
+//     }
+// }
+
+//time complexity:-O(n)
+
+
+
+//Power of a Number with optimized approach.
 import java.util.*;
 
 public class demo4{
 
     public static int display(int x, int n){
-        if(n==0)
+        if(n==0){
             return 1;
-        return x*display(x, n-1);
+        }
+         int pow=display(x, n/2);
+        if(n%2==0){
+            return pow*pow;
+        }
+        else  {
+            return pow*pow*x;     
+        }
     }
 
     public static void main(String[] args){
@@ -21,3 +53,5 @@ public class demo4{
         System.out.println("Result = "+display(x,n));
     }
 }
+
+//time complexity:- O(logn)
