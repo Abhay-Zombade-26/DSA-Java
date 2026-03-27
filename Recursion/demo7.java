765329bc4505395d05282a58e85afb893e4192a3
Abhay9326397439 <abhayzombade26@gmail.com>
2026-03-27 23:48:19 +0530

    print the first k multiples of a number

--- Recursion/demo7.java
+++ Recursion/demo7.java
@@ -1,20 +1,47 @@
-//sum of digits using recursion
+//print the first k multiples of a number using recursion.
 
 package Recursion;
 
 import java.util.*;
 
 public class demo7 {
-    public static int display(int n){
-        if(n>=0 && n<=9){
-            return n;
+    public static void display(int num,int k){
+        if (k==0) {
+            return;
         }
-        return n%10 + display(n/10);
+        display(num, k-1);
+
+        System.out.print(num * k+"  ");
 
     }
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("enter a value of no:");
         int n=sc.nextInt();
-        System.out.println("Sum of digits:"+display(n));
+        System.out.println("enter a value of k:");
+        int k=sc.nextInt();
+        display(n,k);
     }
 }
+
+
+
+//sum of digits using recursion (just solved one more time for practice)
+
+// import java.util.*;
+
+// public class demo7 {
+//     public static int display(int n){
+//         if(n>=0 && n<=9){
+//             return n;
+//         }
+//         return n%10 + display(n/10);
+
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         System.out.println("Sum of digits:"+display(n));
+//     }
+// }
